89a38f0f745ca064d8e7b021455e4ca2245ede49
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-10-13 17:31:10 +0300

    class calculator created

--- src/com/pb/dolgopolyj/hw2/Calculator.java
+++ src/com/pb/dolgopolyj/hw2/Calculator.java
@@ -2,6 +2,7 @@
 package com.pb.dolgopolyj.hw2;
 
 //Для взаимодействия с пользователем импортируем класс Scanner
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 // Создаем класс Calculator
@@ -11,14 +12,15 @@ public class Calculator {
     public static void main(String[] args) {
     //Объявляем две необходимые по заданию целочисленные переменные operand1 и operand2
     int operand1,operand2;
+
     //Создаем строковую переменную для получения типа выполняемой операции
     String sign;
     //Cоздаём объект класса Scanner для обработки ввода с клавиатуры
     Scanner in = new Scanner(System.in);
 
     //Выводим на экран приветствие программы и выполняемые ей действия
-    System.out.println("Добрый день! Это Ваш калькулятор");
-    System.out.println("Им выполняется действие: переменная№1 (операция) переменная№2");
+    System.out.println("Добрый день! \nЭто Ваш 'Калькулятор'");
+    System.out.println("Им выполняется действие: переменная№1 'операция' переменная№2");
     //Cообщаем о типах выполняемых калькулятором операций
     System.out.println("Допустимы операции: +,-,*,/");
 
@@ -36,13 +38,50 @@ public class Calculator {
     //Организовываем проверку операции деления на ноль
    if ((sign.equals("/")&&operand2==0)){
        //при попытке деления на ноль выводим сообщение о недопустимой операции
-       System.out.println("Это недопустимая операция - деление на ноль");
-       System.out.println("Выполнение программы будет прекращено");
+       System.out.println("Это недопустимая операция - деление на ноль \nВыполнение программы будет прекращено");
        }
    else {
        //если недопустимой операции нет, продолжаем выполнение программы
-       System.out.println("Операция с переменными корректна");
-        }
 
-    }
+       //Объявляем и инициализируем переменную для хранения результата вычисления
+       float caclcresult=0;
+       //Объявляем и инициализируем переменную для результата корректности операции
+       boolean correctoperation=true;
+
+       //В зависимости от типа вычисления, получаем результат
+       switch (sign.charAt(0)){
+
+           case ('-'):
+               caclcresult=operand1-operand2;
+               break;
+           case ('+'):
+               caclcresult=operand1+operand2;
+               break;
+           case ('*'):
+               caclcresult=operand1*operand2;
+               break;
+           case ('/'):
+               caclcresult=operand1/operand2;
+               break;
+
+           default:
+               //Если в знак операции введен некорректный символ, вычисления не проводим
+               correctoperation=false;
+       }
+
+       if (correctoperation==true) {
+           //Если операция корректна, отображаем ее результат
+           System.out.println("Операция с переменными корректна");
+           System.out.println("Результат операции:" + operand1 + sign + operand2+"="+caclcresult);
+           }
+       else {
+           //Иначе сообщаем об ошибке
+           System.out.println("Это некорректная операция \nВыполнение программы будет прекращено");
+           }
+
+       }
+
+       //Благодарности :)
+       System.out.println("*** Cпасибо, что используете программу 'Калькулятор' ***");
+   }
 }
