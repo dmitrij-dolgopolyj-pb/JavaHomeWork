7fea92c0bbea0c33361d7b3d05b812e9d13675f1
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-10-31 11:36:35 +0200

    class Library created

--- src/com/pb/dolgopolyj/hw5/Book.java
+++ src/com/pb/dolgopolyj/hw5/Book.java
@@ -10,4 +10,35 @@ public class Book {
     private String bookName;
     private String authorName;
     private int yearPublish;
+
+    //Описываем блок "cеттеров"
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setBookName (String bookName){
+       this.bookName=bookName;
+    }
+
+    //Используем метод setYearPublish для проверки ввода некорректного - например,отрицательного, значения года
+    public void setYearPublish (int yearPublish){
+        if (yearPublish<0) {
+            Library.printError();
+            return;
+        }
+        else this.yearPublish=yearPublish;
+    }
+
+    //Блок "геттеров" сгенерирован в среде разработки автоматически:
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public int getYearPublish() {
+        return yearPublish;
+    }
 }
--- src/com/pb/dolgopolyj/hw5/Library.java
+++ src/com/pb/dolgopolyj/hw5/Library.java
@@ -4,19 +4,51 @@ package com.pb.dolgopolyj.hw5;
  * Класс, демонстрирующий работу классов Book и Reader
  */
 public class Library {
+
+    //Описываем метод, выводящий сообщение о попытке ввода некорректного значения
+    static void printError() {
+        System.out.println("Вы пытаетесь ввести некорректные данные");
+    }
+
     public static void main(String[] args) {
+
+
+
         //Выводим на экран приветствие программы и выполняемые ей действия
         System.out.println("*** Добрый день! ***\nЭто класс 'Library'");
         System.out.println("Он демонстрирует работу классов Book и Reader");
 
-        //Инициализируем массив книг в библиотеке
-        Book book1 = new Book();
-        //book1.bookName="Блицкриг 1941";
-        //book1.authorName="Исаев А.В.";
-        //book1.yearPublish=1998;
-        // System.out.println("Название книги:"+book1.bookName+", автор:"+book1.authorName+", год издания:"+book1.yearPublish);
+        //Инициируем переменную для определения количества книг в библиотеке
+        int numberOffBooks=5;
+        //Инициализируем массив книг в библиотеке, то есть ссылок на объекты класса Book
+        Book [] book = new Book[numberOffBooks];
+        //Теперь инициализируем непосредственно сами "книги", то есть отдельные объекты класса Book
+        for (int i=0;i<numberOffBooks;i++) book[i]= new Book();
+
+
+
+       book[0].setBookName("Знакомьтесь: JAVA");
+       book[0].setAuthorName("Аккуратов Е.Е.");
+       book[0].setYearPublish(2009);
+       book[1].setBookName("Блицкриг 1941");
+       book[1].setAuthorName("Исаев А.В.");
+       book[1].setYearPublish(2001);
+       book[2].setBookName("Приключения");
+       book[2].setAuthorName("Иванов И.И.");
+       book[2].setYearPublish(2000);
+       book[3].setBookName("Словарь");
+       book[3].setAuthorName("Сидоров А.В.");
+       book[3].setYearPublish(1980);
+       book[4].setBookName("Энциклопедия");
+       book[4].setAuthorName("Гусев К.В.");
+       book[4].setYearPublish(2010);
+
+        //Выводим на экран имеющиеся в библиотеке книги
+        for (int i=0;i<book.length;i++) {
+            System.out.println("Название книги:" + book[i].getBookName() + ", автор:" + book[i].getAuthorName() + ", год издания:" + book[i].getYearPublish());
+        }
 
         //Благодарности :)
-        System.out.println("*** Cпасибо за использование класса 'Anagram'! ***");
+        System.out.println("*** Cпасибо за использование класса 'Library'! ***");
     }
 }
