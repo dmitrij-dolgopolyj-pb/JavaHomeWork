9f9dd919aacc271be220d5c27d979f92b6960663
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-11-21 20:55:20 +0200

    HW9 done

--- src/com/pb/dolgopolyj/hw9/FileNumbers.java
+++ src/com/pb/dolgopolyj/hw9/FileNumbers.java
@@ -23,7 +23,7 @@ public class FileNumbers
         String str;
         //Объявляем переменные для работы с циклами
         int i,k;
-        //Объявляем массив для работы с данными
+        //Объявляем массив для работы с данными файла 1
         int [] array = new int[10];
         //Объявляем переменную, которая будет помогать генерировать случайное число
         Random random=new Random();
@@ -32,7 +32,6 @@ public class FileNumbers
         // Рабочие файлы программы будем хранить в папке нашего домашнего задания
         Path path = Paths.get("out\\production\\JavaHomeWork\\com\\pb\\dolgopolyj\\hw9\\numbers.txt");
         //Записываем первый файл
-
         try (BufferedWriter writer = Files.newBufferedWriter(path))
         {
             for (k=0;k<10;k++)
@@ -43,6 +42,7 @@ public class FileNumbers
                     array[i] = random.nextInt(99) + 1;
                     str = str + Integer.toString(array[i]) + " ";
                 }
+                //Записываем полученную строку и переходим на следующую
                 writer.write(str);
                 writer.newLine();
             }
@@ -51,27 +51,53 @@ public class FileNumbers
         {
             System.out.println("Ошибка записи файла №1: " + ex);
         }
+        System.out.println("Запись в файл №1 \"" + path.toAbsolutePath() + "\" прошла успешно!\n");
 
-        System.out.println("Запись в файл №1 \"" + path.toAbsolutePath() + "\" прошла успешно!");
-
-        System.out.println("Read text from file \"" + path.toAbsolutePath() + "\":");
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-
-            System.out.println("----------------------------------");
-            String line;
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
+        //Объявляем массив для работы с данными файла 2
+        int [][] array2 = new int[10][10];
+        //Ещё один строковый массив будем использовать для работы с разделением полученных строк из файла
+        String[] strParts;
+        //Загружаем полученный файл с помощью класса BufferedReader
+        System.out.println("Проводим чтение файла №1: \"" + path.toAbsolutePath() + "\":");
+        try (BufferedReader reader = Files.newBufferedReader(path))
+        {
+            System.out.println("--------------Содержимое файла №1--------------------");
+            k=0;
+            while((str = reader.readLine()) != null)
+            {
+                //Преобразуем полученную строку в строковый массив отдельных чисел, с помощью регулярного выражения (RegEx)
+                //в качестве разделителя слов принимаем пробелы
+                strParts=str.split(" ");
+                for (i=0;i<strParts.length;i++)
+                {
+                    //Преобразуем "cтроковые" числа из полученной строки в элементы массива array2, формата int
+                    array2[i][k]=Integer.valueOf(strParts[i]);
+                    System.out.print(array2[i][k] + " ");
+                }
+                k++;
+                System.out.print("\n");
             }
             System.out.println("----------------------------------");
 
-        } catch (Exception ex) {
-            System.out.println("Error with file read: " + ex);
         }
-        System.out.println("Read from file done!");
+        catch (Exception ex)
+        {
+            System.out.println("Ошибка чтения файла №1: " + ex);
+        }
+        System.out.println("Файл №1 прочитан успешно!\n");
 
-
-
-
+        System.out.println("----------Данные для файла №2------------------------");
+        //Осуществляем проверку полученного массива на четные числа и заменим их на "0"
+        for (k=0;k<10;k++)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                if (array2[i][k]%2==0) array2[i][k]=0;
+                System.out.print(array2[i][k] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("----------------------------------\n");
 
         //Записываем второй файл
         path = Paths.get("out\\production\\JavaHomeWork\\com\\pb\\dolgopolyj\\hw9\\odd-numbers.txt");
@@ -79,20 +105,19 @@ public class FileNumbers
             for (k=0;k<10;k++)
             {
                 str = "";
-                for (i = 0; i < 10; i++) {
-                    //Генерируем случайное число от 1 до 99
-                    array[i] = random.nextInt(99) + 1;
-                    str = str + Integer.toString(array[i]) + "-";
+                for (i = 0; i < 10; i++)
+                {
+                    str = str + Integer.toString(array2[i][k]) + " ";
                 }
                 writer.write(str);
                 writer.newLine();
             }
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             System.out.println("Ошибка записи файла №2: " + ex);
         }
-
-        System.out.println("Запись в файл №2 \"" + path.toAbsolutePath() + "\" прошла успешно!");
-
+        System.out.println("Запись в файл №2 \"" + path.toAbsolutePath() + "\" прошла успешно!\n");
 
         //Благодарности :)
      System.out.println("\n*** Cпасибо за использование класса 'FileNumbers'!!! ***");
