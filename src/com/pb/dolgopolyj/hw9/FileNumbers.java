9cbfc7eb3c5fc880056b71638af4df1191e938ce
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-11-21 10:18:49 +0200

    HW9 do

---
+++ src/com/pb/dolgopolyj/hw9/FileNumbers.java
@@ -0,0 +1,18 @@
+package com.pb.dolgopolyj.hw9;
+
+/**
+ * Класс FileNumbers, который демонстрирует работу с файлами.
+ */
+public class FileNumbers
+{
+    public static void main(String[] args)
+    {
+    //Выводим на экран приветствие программы и выполняемые ей действия
+    System.out.println("*** Добрый день! ***\nВас приветствует класс 'FileNumbers'!!!");
+    System.out.println("Он продемонстрирует работу с файлами.");
+
+
+     //Благодарности :)
+     System.out.println("\n*** Cпасибо за использование класса 'FileNumbers'!!! ***");
+    }
+}
