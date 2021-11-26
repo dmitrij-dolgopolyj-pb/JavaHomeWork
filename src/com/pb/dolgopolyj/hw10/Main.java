eb6b78f1714ee6e06be3bab3fac778a21b14ceca
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-11-26 21:32:04 +0200

    NumBox checked

--- src/com/pb/dolgopolyj/hw10/Main.java
+++ src/com/pb/dolgopolyj/hw10/Main.java
@@ -16,14 +16,21 @@ public class Main
         //Инициализируем переменную, отвечающую за размерность нашего массива
         int arrayDepth=3;
         NumBox myNumBox= new NumBox (arrayDepth);
-        Number myNumber=3;
-        Integer myNumber2=4;
-        Float myNumber3=4.358F;
+        Long myNumber1=23474L;
+        Integer myNumber2=48798;
+        Float myNumber3=6874.358F;
+        Integer myNumber4=5;
         //Наполняем массив значениями
         try
         {
-            myNumBox.add(myNumber);
+            myNumBox.add(myNumber1);
+            System.out.println("Теперь количество элементов в нашем массиве равно:"+myNumBox.length()+"шт.");
             myNumBox.add(myNumber2);
+            System.out.println("Теперь количество элементов в нашем массиве равно:"+myNumBox.length()+"шт.");
+            myNumBox.add(myNumber3);
+            System.out.println("Теперь количество элементов в нашем массиве равно:"+myNumBox.length()+"шт.");
+
+            //Демонстрация вызова исключения переполнения массива
             myNumBox.add(myNumber3);
         }
         //Если мы попытались выйти при наполнении массива за его границы, ловим соответствующее исключение
@@ -32,8 +39,14 @@ public class Main
             System.out.println("Вы не можете наполнить массив этим элементом," +
                     " общая размерность массива "+arrayDepth+" элемента(ов)!");
         }
-
-
+        System.out.println("В нашем массиве находятся следующие элементы:");
+        for (int i=0; i<arrayDepth;i++)
+        {
+           System.out.println("myNumBox["+i+"]="+myNumBox.get(i));
+        }
+        System.out.println("Cумма всех элементов нашего массива равна:"+myNumBox.sum());
+        System.out.println("Cреднее арифметическое нашего массива равно:"+myNumBox.average());
+        System.out.println("Максимальный элемент нашего массива это:"+myNumBox.max());
 
 
 
--- src/com/pb/dolgopolyj/hw10/NumBox.java
+++ src/com/pb/dolgopolyj/hw10/NumBox.java
@@ -19,6 +19,8 @@ public class NumBox<T extends Number>
         //Инициализируем массив объектов класса T
         numBoxArray= (T[]) new Number[arrayMaxLenght];
     }
+
+    //Метод, добавляющий число в массив
     void add(T num) throws WrongNumberArrayElements
     {
         //При попытке записать значение в несуществующий элемент массива, выбрасываем исключение
@@ -27,4 +29,52 @@ public class NumBox<T extends Number>
         System.out.println("В массив T["+currentElemArray+"] добавлено значение:"+numBoxArray[currentElemArray]);
         currentElemArray++;
     }
+
+    //Метод, возвращающий число по индексу
+    T get(int index)
+    {
+        return numBoxArray[index];
+    }
+
+    //Метод, возвращающий текущее количество элементов.
+    int length()
+    {
+        return currentElemArray;
+    }
+
+    //Метод, который ведет подсчет суммы всех элементов массива.
+    double sum()
+    {
+        double sum=0;
+        for (int i=0;i<currentElemArray;i++)
+        {
+            sum+=(numBoxArray[i]).doubleValue();
+        }
+        return sum;
+    }
+
+    //Метод, который ведет подсчет среднего арифметического среди элементов массива.
+    double average()
+    {
+        double average=0;
+        average=this.sum()/currentElemArray;
+        return average;
+    }
+
+    //Метод, который находит максимальный элемент массива.
+    T max()
+    {
+        int maxNumber=0;
+        double maxValue=(numBoxArray[maxNumber]).doubleValue();
+        for (int i=0;i<currentElemArray;i++)
+        {
+            if ((numBoxArray[i]).doubleValue()>maxValue)
+            {
+                maxValue=(numBoxArray[i]).doubleValue();
+                maxNumber=i;
+            }
+        }
+        return numBoxArray[maxNumber];
+    }
+
 }
