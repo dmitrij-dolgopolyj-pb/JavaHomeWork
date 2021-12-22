26e09ca94f56fd40852696e4545aaeb78d5240ff
Dmitrij Dolgopolyj <dmitdol@gmail.com>
2021-12-22 21:27:41 +0200

    HW13 in work 1

--- src/com/pb/dolgopolyj/hw13/TwoThreads.java
+++ src/com/pb/dolgopolyj/hw13/TwoThreads.java
@@ -1,18 +1,27 @@
 package com.pb.dolgopolyj.hw13;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 /**
  * Класс TwoThreads, который организовывает работу с двумя потоками, разделяющих общий буфер данных
  */
 public class TwoThreads
 {
-    //Для создания одного из потоков создаем свой класс MyThread1, наследника класса Thread
-    static class MyThread1 extends Thread
+    //Переменная класса
+    private static volatile int myValue = 0;
+
+    //Для создания одного из потоков создаем свой класс MyThread1
+    private static class MyThread1 implements Runnable
     {
-        //Конструктор класса
-        public MyThread1()
+        private int x;
+        List<Integer> dataBuffer;
+
+        public MyThread1(List<Integer> dataBuffer)
         {
+            this.dataBuffer = dataBuffer;
         }
+
         //Переопределим метод run этого потока - он будет генерировать случайные числа
         @Override
         public void run()
@@ -20,25 +29,42 @@ public class TwoThreads
             //Объявляем переменную, которая будет помогать генерировать случайное число
             Random random=new Random();
             int rundomNumber;
+            x=0;
             for (int i = 0; i < 10; i++)
             {
                 //Организуем задержку работы потока
                 for (int k = 0; k < 1000000; k++)
-                { rundomNumber=random.nextInt(11);  }
+                {rundomNumber=random.nextInt(11);  }
                 //Генерируем случайное число от 0 до 10
                 rundomNumber=random.nextInt(11);
-                System.out.println(getName() + " iteration: " + (i + 1)+"rundom="+rundomNumber);
+                System.out.println("Производитель сгенерировал число," + " iteration: " + (i + 1)+"rundom="+rundomNumber);
+                dataBuffer.add(rundomNumber);
+                System.out.println("Число:"+rundomNumber+" записано в dataBuffer");
+                myValue=myValue+1;
+                x=myValue+2;
+                System.out.println("myValue="+myValue+"; x="+x);
             }
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
         }
     }
 
-    //Для создания второго потока создаем свой класс MyThread2, наследника класса Thread
-    static class MyThread2 extends Thread
+    //Для создания второго потока создаем свой класс MyThread2
+    private static class MyThread2 implements Runnable
     {
+        List<Integer> dataBuffer;
         //Конструктор класса
-        public MyThread2()
+        public MyThread2(List<Integer> dataBuffer)
         {
+            this.dataBuffer = dataBuffer;
         }
+
         //Переопределим метод run этого потока - он будет генерировать случайные числа
         @Override
         public void run()
@@ -52,8 +78,29 @@ public class TwoThreads
                 for (int k = 0; k < 1000000; k++)
                 {rundomNumber=random.nextInt(11);  }
                 //Генерируем случайное число от 11 до 20
-                rundomNumber=random.nextInt(11)+11;
-                System.out.println(getName() + " Потребляю на iteration: " + (i + 1)+"rundom="+rundomNumber);
+                rundomNumber=random.nextInt(11)+10;
+                System.out.println("Потребляю на iteration: " + (i + 1)+"rundom="+rundomNumber);
+                myValue=myValue+100;
+                System.out.println("Потребитель изменил внутреннюю переменную, myValue="+myValue);
+                if (dataBuffer.size()>0)
+                {
+                    System.out.println("В буфере есть число, dataBuffer[0]="+dataBuffer.get(0));
+                }
+//                // Проверяем, работает ли ещё "производитель"
+//                if (manufacturerThread.isAlive()) {
+//                    try {
+//                        // Если "производитель" еще работает, ждем окончания его работы
+//                        manufacturerThread.join();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    System.out.println("Ожидаю окончания работы производителя...");
+//
+//                } else {
+//                    //если "производитель" уже закончил работу, спокойно завершаем свою
+//                    System.out.println("Производитель уже завершил работу, можно заканчивать свою.");
+//                }
             }
         }
     }
@@ -67,14 +114,23 @@ public class TwoThreads
         System.out.println("Поток 'Производитель' генерирует данные (случайные числа от 1 до 100) и помещает их в буфер.");
         System.out.println("Поток 'Потребитель' «потребляет» их из буфера - выводит на печать в консоль и очищает буфер.");
 
+        List<Integer> dataBuffer=new ArrayList<>();
+
         //Создаем объект класса MyThread1 для потока-производителя
-        Thread manufacturerThread=new MyThread1();
+        //Thread manufacturerThread=new MyThread1();
+        MyThread1 manufacturerThread=new MyThread1(dataBuffer);
+        Thread t1 = new Thread(manufacturerThread, "t1");
         //Создаем объект класса MyThread2 для потока-потребителя
-        Thread consumerThread=new MyThread2();
+        //Thread consumerThread=new MyThread2();
+        MyThread2 consumerThread=new MyThread2(dataBuffer);
+        Thread t2 = new Thread(consumerThread, "t2");
         //Запускаем поток-производитель
-        manufacturerThread.start();
+        t1.start();
         //Запускаем поток-потребитель
-        consumerThread.start();
+        t2.start();
+
+        System.out.println("Переменная х 'производителя' равна:"+manufacturerThread.getX());
+        System.out.println("Переменная myValue равна:"+myValue);
 
         //Благодарности :)
         System.out.println("\n************************** Cпасибо за использование нашей 'TwoThreads'!!! ************************");
